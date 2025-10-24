568d63d53eb2d4226daf311e213413117831319e
Rawda Mohamed <rawdaabdelkhalek123@gmail.com>
2025-10-24 18:30:25 +0300

    add home tan add home drawer add categories view add sources view

---
+++ lib/features/home/sources_view/sources_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/category_model.dart';
+
+class SourcesView extends StatelessWidget {
+  final CategoryModel category;
+  const SourcesView({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color:  Colors.red);
+  }
+}
